86569e5035dc0ebfb088c71a056d6ac7f57d7eeb
Mohammed <mohammed@loud-20.04>
2021-09-16 02:24:38 -0400

    Lab3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './header/top-nav.bar.component';
+import { ImagesGalleryComponent } from './gallery/images-gallery.component';
+import { PurchaseItemsComponent } from './items/purchase-items.component';
+import { PurchaseItems2Component } from './items2/purchase-items2.component';
+import { LayoutMainComponent } from './layout/layout-main.component';
+import { BottomNavBarComponent } from './footer/bottom-nav-bar.component';
+import { DownAdvComponent } from './Advertisment/Down-Adv.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    ImagesGalleryComponent,
+    PurchaseItemsComponent,
+    LayoutMainComponent,
+    PurchaseItems2Component,
+    DownAdvComponent,
+    BottomNavBarComponent
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/items/purchase-items.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-purchase-items',
+    templateUrl: 'purchase-items.component.html'
+})
+
+export class PurchaseItemsComponent { 
+
+}
