50999c91896cf86289f6df1d444a453f65b04f91
msa0041 <89413281+msa0041@users.noreply.github.com>
2021-09-16 17:08:40 -0400

    aded

---
+++ ebay/src/app/gallery/images-gallery.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-images-gallery',
+    templateUrl: 'images-gallery.component.html'
+})
+
+export class ImagesGalleryComponent { 
+
+}
