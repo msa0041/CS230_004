55cf5e1bc0fe831511c3e9b8f1961a36848577d9
Mohammed <mohammed@loud-20.04>
2021-09-16 14:26:50 -0400

    Lab 3

--- ebay/src/app/app.module.ts
+++ ebay/src/app/app.module.ts
@@ -5,7 +5,12 @@ import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './header/top-nav.bar.component';
 import { ImagesGalleryComponent } from './gallery/images-gallery.component';
 import { PurchaseItemsComponent } from './items/purchase-items.component';
+import { PurchaseItems2Component } from './items2/purchase-items2.component';
+import { LayoutMainComponent } from './layout/layout-main.component';
 import { BottomNavBarComponent } from './footer/bottom-nav-bar.component';
+import { DownAdvComponent } from './Advertisment/Down-Adv.component';
+
+
 
 
 @NgModule({
@@ -14,6 +19,9 @@ import { BottomNavBarComponent } from './footer/bottom-nav-bar.component';
     TopNavBarComponent,
     ImagesGalleryComponent,
     PurchaseItemsComponent,
+    LayoutMainComponent,
+    PurchaseItems2Component,
+    DownAdvComponent,
     BottomNavBarComponent
 
   ],
---
+++ ebay/src/app/items2/purchase-items2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-purchase-items2',
+    templateUrl: 'purchase-items2.component.html'
+})
+
+export class PurchaseItems2Component { 
+shop:string = "Today`s Deals - All With Free Shipping"
+}
